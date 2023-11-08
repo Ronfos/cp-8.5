13c7ae47e1ea53c20c89034ac84a93cd60c7d171
Ronfos <74715764+Ronfos@users.noreply.github.com>
2023-11-08 19:37:42 +0200

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        //Вариант 1 (Уровень А)
+        //static void Function(double a = 1,double b = 2, double c = 3)
+        //{
+        //    double y = ((a + Math.Sin(a)) / 3) + ((b + Math.Sin(b)) / 3) + ((c + Math.Sin(c)) / 3);
+        //    Console.Write($"{y:f2}");
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Function(1,5,7);
+        //    Console.ReadKey();
+        //}
+
+        //Вариант 2 (Уровень А)
+        static void Secondscount(int h = 1, int m = 10, int s = 20)
+        {
+            int seconds = h * 60 * 60 + m * 60 + s;
+            Console.Write($"Общее количество секунд:{seconds}");
+        }
+        static void Main()
+        {
+            Console.Write("Введите часы: ");
+            int h = int.Parse(Console.ReadLine());
+            Console.Write("Введите минуты: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите секунды: ");
+            int s = int.Parse(Console.ReadLine());
+            Secondscount(h, m, s);
+            Console.ReadKey();
+        }
+
+    }
+}
